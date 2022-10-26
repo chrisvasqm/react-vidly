67f76fd530e88778b45e3653d0948412448caad0
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-26 15:45:42 -0400

    Add empty state

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,41 +3,50 @@ import { getMovies } from "../services/fakeMovieService.js";
 
 class Movies extends Component {
   state = {
-    movies: getMovies(),
+    movies: [],
   };
 
   render() {
     return (
-      <div>
-        <main className="container">
-          <h1>Movies</h1>
+      <main className="container">
+        <h1>Movies</h1>
 
-          <p>Showing {this.state.movies.length} movies from the database</p>
+        {this.renderMovies()}
+      </main>
+    );
+  }
 
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope='col'>Title</th>
-                <th scope='col'>Genre</th>
-                <th scope='col'>Stock</th>
-                <th scope='col'>Rate</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.movies.map((m) => {
-                return (
+  renderMovies() {
+    const isEmpty = this.state.movies.length === 0;
+    if (isEmpty) return <p>There are no movies in the database</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {this.state.movies.length} movies from the database</p>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((m) => {
+              return (
                 <tr key={m._id}>
                   <td>{m.title}</td>
                   <td>{m.genre.name}</td>
                   <td>{m.numberInStock}</td>
                   <td>{m.dailyRentalRate}</td>
                 </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        </main>
-      </div>
+              );
+            })}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
