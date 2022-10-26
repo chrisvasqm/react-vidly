69a7f77b3326bdf21a579c127ac1b27be1229a48
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-26 15:35:28 -0400

    Render table headers

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -13,6 +13,17 @@ class Movies extends Component {
           <h1>Movies</h1>
 
           <p>Showing {this.state.movies.length} movies from the database</p>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope='col'>Title</th>
+                <th scope='col'>Genre</th>
+                <th scope='col'>Stock</th>
+                <th scope='col'>Rate</th>
+              </tr>
+            </thead>
+          </table>
         </main>
       </div>
     );
