916e0cd3d7641083063746d56ffc98b503340ff8
chrisvasqm <christianvasquezmelo@gmail.com>
2022-11-01 11:56:16 -0400

    Import library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Movies from "./components/movies";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
