31ddadfb39033af41d7a561b21b5452dc3cdc55e
chrisvasqm <christianvasquezmelo@gmail.com>
2022-11-04 10:50:18 -0400

    Fix for page count lower than 1

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,7 +2,8 @@ import _ from "lodash";
 
 const Pagination = props => {
   const { itemsCount, pageSize } = props;
-  const pagesCount = itemsCount / pageSize;
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
 
   return (
