7372feb914461fd0787031831987e6da56f4c6e1
chrisvasqm <christianvasquezmelo@gmail.com>
2023-03-28 21:01:35 -0400

    Set grid layout

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -30,57 +30,62 @@ class Movies extends Component {
     const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
-      <React.Fragment>
-        <p>Showing {count} movies from the database</p>
+      <React.Fragment className='container'>
+        <div className='row'>
+          <div className='col-2'>Categories</div>
+          <div className='col'>
+            <p>Showing {count} movies from the database</p>
 
-        <table className='table'>
-          <thead>
-            <tr>
-              <th scope='col'>Title</th>
-              <th scope='col'>Genre</th>
-              <th scope='col'>Stock</th>
-              <th scope='col'>Rate</th>
-              <th scope='col'>Liked</th>
-              <th scope='col'></th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map(movie => {
-              return (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      isLiked={movie.isLiked}
-                      onClick={() => {
-                        this.handleLike(movie);
-                      }}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      className='btn btn-danger btn-sm m-2'
-                      onClick={() => {
-                        this.handleDelete(movie._id);
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </td>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th scope='col'>Title</th>
+                  <th scope='col'>Genre</th>
+                  <th scope='col'>Stock</th>
+                  <th scope='col'>Rate</th>
+                  <th scope='col'>Liked</th>
+                  <th scope='col'></th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <Pagination
-          itemsCount={count}
-          pageSize={this.state.pageSize}
-          currentPage={this.state.currentPage}
-          onPageChanged={this.handlePageChage}
-        />
+              </thead>
+              <tbody>
+                {movies.map(movie => {
+                  return (
+                    <tr key={movie._id}>
+                      <td>{movie.title}</td>
+                      <td>{movie.genre.name}</td>
+                      <td>{movie.numberInStock}</td>
+                      <td>{movie.dailyRentalRate}</td>
+                      <td>
+                        <Like
+                          isLiked={movie.isLiked}
+                          onClick={() => {
+                            this.handleLike(movie);
+                          }}
+                        />
+                      </td>
+                      <td>
+                        <button
+                          className='btn btn-danger btn-sm m-2'
+                          onClick={() => {
+                            this.handleDelete(movie._id);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <Pagination
+              itemsCount={count}
+              pageSize={this.state.pageSize}
+              currentPage={this.state.currentPage}
+              onPageChanged={this.handlePageChage}
+            />
+          </div>
+        </div>
       </React.Fragment>
     );
   }
